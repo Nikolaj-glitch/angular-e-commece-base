8ad87575c5a5964e728b92d8713015fa2016c0fb
Nikolaj-glitch <75386723+Nikolaj-glitch@users.noreply.github.com>
2025-05-22 17:39:21 +0200

    Aggiustato il minimo necessario il css di products-component

--- src/app/features/products/products/products.component.ts
+++ src/app/features/products/products/products.component.ts
@@ -1,39 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 import { NgFor } from '@angular/common';
 import { Product, ProductService } from './products.services';
 @Component({
   selector: 'app-products',
   standalone: true,
   imports: [
+    MatDividerModule,
     MatCardModule,
     NgFor
   ],
   styleUrl: './products.component.scss',
   template: `
-    <button (click)="logout()">Logout</button>
     <div>
-      <h1>Prodotti</h1>
+  <button (click)="logout()">Logout</button>
+  <h1>Prodotti</h1>
 
-      <mat-card class="maxSizeMatCard" *ngFor="let product of products">
-        <mat-card-header>
-          <mat-card-title>{{ product.name }}</mat-card-title>
-          <mat-card-subtitle>{{ product.price}}</mat-card-subtitle>
-        </mat-card-header>
+  <div class="product-grid">
+    <mat-card class="product-card" *ngFor="let product of products">
+      <mat-card-header>
+        <mat-card-title>{{ product.name }}</mat-card-title>
+        <mat-card-subtitle>{{ product.price }}</mat-card-subtitle>
+      </mat-card-header>
 
-        <img mat-card-image [src]="product.imageUrl" alt="{{ product.name }}">
+      <img mat-card-image [src]="product.imageUrl" alt="{{ product.name }}">
 
-        <mat-card-content>
-          <p>{{ product.description }}</p>
-          <p>Disponibilità: {{ product.stock }}</p>
-        </mat-card-content>
+      <mat-card-content>
+        <p>{{ product.description }}</p>
+        <p>Disponibilità: {{ product.stock }}</p>
+      </mat-card-content>
 
-        <mat-card-actions>
-          <button mat-button>Aggiungi al carrello</button>
-        </mat-card-actions>
-      </mat-card>
-    </div>
+      <mat-card-actions>
+        <button mat-button>Aggiungi al carrello</button>
+      </mat-card-actions>
+    </mat-card>
+  </div>
+</div>
   `
 })
 export class ProductsComponent implements OnInit {
